075a54bad33745cb7b479f665063156875b4338a
philippedevloo <philippedevloo1958@gmail.com>
2018-09-24 11:11:22 -0300

    Implementing intersection one element at a time

---
+++ MHM_Frac/TPZElementIntersect.cpp
@@ -0,0 +1,8 @@
+//
+//  TPZElementIntersect.cpp
+//  FractureIntersection
+//
+//  Created by Philippe Devloo on 11/09/18.
+//
+
+#include "TPZElementIntersect.h"
---
+++ MHM_Frac/VTKIntersect.cpp
@@ -0,0 +1,8 @@
+//
+//  VTKIntersect.cpp
+//  Benchmark0a
+//
+//  Created by Philippe Devloo on 21/09/18.
+//
+
+#include "VTKIntersect.hpp"
